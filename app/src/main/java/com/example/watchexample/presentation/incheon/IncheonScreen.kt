0d0ff0e7ed4a02588d91c25d7ca24347a67c7c08
권혁준 <khjunejune10@gmail.com>
2023-03-15 00:36:25 +0900

    TitleCard 변경

--- app/src/main/java/com/example/watchexample/presentation/incheon/IncheonScreen.kt
+++ app/src/main/java/com/example/watchexample/presentation/incheon/IncheonScreen.kt
@@ -8,12 +8,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.wear.compose.material.AppCard
-import androidx.wear.compose.material.MaterialTheme
-import androidx.wear.compose.material.Text
+import androidx.wear.compose.material.*
+import com.example.watchexample.R
 
 @Composable
 fun IncheonScreen(
@@ -24,12 +24,13 @@ fun IncheonScreen(
         modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
     ) {
-        AppCard(
+        TitleCard(
             modifier = modifier,
-            appName = { Text(text = "지역 날씨") },
-            time = { Text(text = "맑음")},
-            title = { Text(text = "Incheon") },
-            contentColor = Color.Green,
+            time = { },
+            title = { Text(text = "Incheon Weather") },
+            backgroundPainter = CardDefaults.imageWithScrimBackgroundPainter(
+                backgroundImagePainter = painterResource(id = com.google.android.gms.base.R.drawable.common_full_open_on_phone)
+            ),
             onClick = { navigateToBack() }
         ) {
             Column {
